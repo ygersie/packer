7f8be7446e9a625c3abfd1e64076ca25515d79c4
Yorick Gersie <yorick+git@gersie.eu>
2021-12-21 16:48:47 +0100

    improve feedback

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -6,23 +6,30 @@ log () {
 }
 
 apt-get update
+log "Setting up python venv"
 DEBIAN_FRONTEND=noninteractive apt-get -yq install python3.8-venv
 
 [[ -f /etc/profile.d/python-venv.sh ]] || { echo "python venv profile missing"; exit 1; }
 . /etc/profile.d/python-venv.sh
 
 mkvenv salt
+log "Installing salt in venv"
 pip install salt
 deactivate
 
+log "Cloning salt repo"
 git clone https://github.com/ygersie/salt.git /srv/salt
 chmod 750 /srv/salt
 
+log "Calling salt-apply"
 count=1
 while [[ ${count} -le 3 ]]; do
-    if /usr/local/bin/salt-apply 2>&1 | egrep -q 'Failed:\s+0'; then
+    output=$(/usr/local/bin/salt-apply 2>&1)
+    if echo "${output}" | egrep -q 'Failed:\s+0'; then
         log "Performed successful salt-call run #${count}.."
         (( count++ ))
+    else
+      echo "${output}"
     fi
 done
 log "Finished 3 successful salt-call runs"
